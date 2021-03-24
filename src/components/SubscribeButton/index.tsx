5330341a323332ccedecf7211e1c06342aac0c52
Mauricio Nogueira Lopes <mauuricionlopes@gmail.com>
2021-03-24 18:24:21 -0300

    finishing ignews project

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { api } from '../../services/api'
+import { getStripeJS } from '../../services/stripe-js'
+import { CustomSession } from '../../types'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +10,38 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+  const router = useRouter()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    if ((session as CustomSession)?.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const res = await api.post('/subscribe')
+
+      const { sessionId } = res.data
+
+      const stripe = await getStripeJS()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+  
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
